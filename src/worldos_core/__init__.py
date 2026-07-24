869992daa7ea3c3692221c5d5900cdc935496586
yet-tang <zijun.xuzj@gmail.com>
2026-07-24 15:12:43 +0800

    feat(inspector): export debug API

--- src/worldos_core/__init__.py
+++ src/worldos_core/__init__.py
@@ -1,6 +1,7 @@
 """WorldOS event-sourced architecture kernel."""
 
 from .events import Event, NewEvent
+from .inspector import ActorDebugView, TimelineSnapshot, WorldInspector
 from .intents import Intent, ValidationIssue, ValidationResult
 from .knowledge import Belief, KnowledgeProjection, Observation, replay_knowledge
 from .memory import MemoryEngine, MemoryPolicy, MemoryProjection, MemoryRecord, replay_memory
@@ -20,5 +21,6 @@ __all__ = [
     "PlanStep", "PlannerProjection", "PlanningContext", "GoalPlanner", "replay_planning",
     "ModuleContext", "WorldModule", "BaseWorldModule", "WorldModuleRegistry",
     "DeterministicTickEngine", "TickAlreadyCompletedError", "TickResult",
+    "TimelineSnapshot", "ActorDebugView", "WorldInspector",
     "InMemoryEventStore", "WorldProjection", "replay_world",
 ]
