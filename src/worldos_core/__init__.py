21a5e80f5c5958c6cdb11948ee93b17cd68d6759
yet-tang <zijun.xuzj@gmail.com>
2026-07-24 10:10:02 +0800

    feat(knowledge): export perception APIs

--- src/worldos_core/__init__.py
+++ src/worldos_core/__init__.py
@@ -2,6 +2,8 @@
 
 from .events import Event, NewEvent
 from .intents import Intent, ValidationIssue, ValidationResult
+from .knowledge import Belief, KnowledgeProjection, Observation, replay_knowledge
+from .perception import PerceptionEngine
 from .pipeline import IntentPipeline, IntentProcessingResult
 from .store import InMemoryEventStore
 from .world import WorldProjection, replay_world
@@ -14,6 +16,11 @@ __all__ = [
     "ValidationResult",
     "IntentPipeline",
     "IntentProcessingResult",
+    "Observation",
+    "Belief",
+    "KnowledgeProjection",
+    "PerceptionEngine",
+    "replay_knowledge",
     "InMemoryEventStore",
     "WorldProjection",
     "replay_world",
