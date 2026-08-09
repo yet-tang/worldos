f98b7c9a7c1785e2d244409077a544a821aee228
yet-tang <zijun.xuzj@gmail.com>
2026-08-09 21:27:23 +0800

    feat: serve extended Chinese world console

--- src/worldos_core/web_inspector_cli.py
+++ src/worldos_core/web_inspector_cli.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import argparse
 
-from .web_console import serve_world_console
+from .web_console_extensions import serve_world_console
 
 
 def main() -> None:
