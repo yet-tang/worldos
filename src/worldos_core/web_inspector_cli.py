f28efe0c39668f04d442f5b181a4c77cacf68fe2
yet-tang <zijun.xuzj@gmail.com>
2026-08-10 00:56:11 +0800

    feat: serve story-aware console

--- src/worldos_core/web_inspector_cli.py
+++ src/worldos_core/web_inspector_cli.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import argparse
 
-from .web_console_summary import serve_world_console
+from .web_console_story import serve_world_console
 
 
 def main() -> None:
